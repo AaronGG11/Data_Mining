b7666b07a751d47afc1509074f990b6253afef65
AaronGG11 <aarongarcia.ipn.escom@gmail.com>
2021-04-27 00:21:23 -0500

    testing dataframe  date

--- Practices/P4-ETL/script1.py
+++ Practices/P4-ETL/script1.py
@@ -1,11 +1,31 @@
 import os
 import csv
+import datetime
 import pandas as pd 
-import numpy as np
+'''
+    SCRIPT FOR CLEANING, TRANSFORMING AND VALIDATE THE DATA
+'''
 
 
 def main():
-    print("Hello world")
+    # creating a dictionary containing a date
+    dict = {'Date':["2020-06-17", "2020-01-14", 
+                    "2020-09-20", "2020-08-15"]}
+    
+    # converting the dictionary to a 
+    # dataframe
+    df = pd.DataFrame.from_dict(dict)
+    
+    # converting the date to the required 
+    # format
+    df['Date'] = pd.to_datetime(df['Date'],
+                                errors ='coerce')
+    df.astype('int64').dtypes
+    
+    # extracting the week from the date
+    weekNumber = df['Date'].dt.week
+    
+    print(weekNumber)
 
 if __name__ == "__main__":
     main()
